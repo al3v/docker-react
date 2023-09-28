eaa84d83bedf3da10d5deaccba37299b137ca1c7
al3v <29979245s@gmail.com>
2023-09-28 17:27:46 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed on the feature branch <3
         </a>
       </header>
     </div>
